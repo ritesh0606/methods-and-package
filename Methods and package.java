257c108bc92044636f3d6a6bf06985d1668511fb
ritesh0606 <78818574+ritesh0606@users.noreply.github.com>
2021-08-29 13:38:02 +0530

    Update Methods and package.java

--- Methods and package.java
+++ Methods and package.java
@@ -1,59 +1,15 @@
-/*
-
- * To change this license header, choose License Headers in Project Properties.
-
- * To change this template file, choose Tools | Templates
-
- * and open the template in the editor.
-
- */
-
-package tasks;
-
 import java.util.Scanner;
-
-/**
-
- *
-
- *
-
- */
-
-public class Tasks {
-
-    /**
-
-     * @param args the command line arguments
-
-     */
-
-    public static void main(String[] args) {
-
-        // TODO code application logic here
-
-        int mins;
-
-        int years;
-
-        int days;
-
-        System.out.println("Enter no. of mins.");
-
-        Scanner s = new Scanner(System.in);
-
-        mins = s.nextInt();
-
-        years = mins/525600;
-
-        days = mins%525600;
-
-        System.out.print("years :"+years);
-
-        System.out.println("\tdays :"+days);
-
-    }
-
-    
-
+public class Main
+{
+	public static void main(String[] args) {
+		int mins;
+		int years;
+		int days;
+		System.out.println("Enter no. of minutes : ");
+		Scanner s = new Scanner(System.in);
+		mins = s.nextInt();
+		years = mins/(60*24*365);
+		days = (mins/60/24)%365;
+		System.out.println(mins + " minutes is approximately "+ years +" years and "+ days + " days.");
+	}
 }
